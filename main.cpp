9f45d1d457cfc6ea4a32335ad781e10ac6799c4f
Marcin Kalemba <252330@edu.p.lodz.pl>
2023-11-19 15:05:16 +0100

    Rozszerzono działanie

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int a=0, b=2;
+    int a=0, b=0;
     cout<<"Podaj liczbe";
     cin>>a;
+    cout<<"Podaj liczbe";
+    cin>>b;
     cout<<a+b;
     return 0;
 }
